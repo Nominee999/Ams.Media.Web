d42368a5cae50d68311912a6bfa9ce8cc1e2d613
Akarin.U <akarin.u@live.com>
2025-09-26 13:24:48 +0700

    feat(client): add blank Client page (controller+view)

---
+++ Controllers/Client/ClientController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ams.Media.Web.Controllers
+{
+    [Authorize]
+    public class ClientController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Client Master";
+            return View();
+        }
+    }
+}
